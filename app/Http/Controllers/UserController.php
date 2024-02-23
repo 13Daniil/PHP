096c1fdf8774932adc9aa4d10a37a97d6a609f64
13Daniil <139341732+13Daniil@users.noreply.github.com>
2024-02-23 20:31:04 +0600

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -29,7 +29,7 @@ class UserController extends Controller
     {
         $credentials = $request->only('name', 'email', 'password');
 
-        if(Auth::attemt($credentials))
+        if(Auth::attempt($credentials))
         {
             return "Успешная аутентификация.";
         }
