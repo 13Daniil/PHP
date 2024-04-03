33f309adf59b8c9f22a41406e46266c22cf795ec
13Daniil <139341732+13Daniil@users.noreply.github.com>
2024-04-03 19:18:14 +0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,24 +2,27 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PhotoController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\PhotoController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-Route::get('/photo/{id}', [PhotoController::class, 'Show']);
-Route::post('/photo/addphoto', [PhotoController::class, 'AddPhoto']);
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/login', [UserController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/post/create', [PostController::class, 'create']);
+Route::get('/post/show', [PostController::class, 'show']);
+Route::delete('/post/{id}', [PostController::class, 'delete']);
+
+Route::post('/comment/create', [CommentController::class, 'create']);
+Route::delete('/comment/{id}', [CommentController::class, 'delete']);
+
+Route::get('/photo/{id}', [PhotoController::class, 'Show']);
+Route::post('/photo/addphoto', [PhotoController::class, 'addPhoto']);
+
+// Route::middleware('auth:sanctum')->group(function () {
+//     Route::post('/post/create', [PostController::class, 'create']);
+//     Route::delete('/post/{id}', [PostController::class, 'delete']);
+//     Route::post('/comment/create', [CommentController::class, 'create']);
+//     Route::delete('/comment/{id}', [CommentController::class, 'delete']);
+// });
