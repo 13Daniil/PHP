eada8847b88c9bb30980c1e4f8cb28d4978205b7
13Daniil <139341732+13Daniil@users.noreply.github.com>
2024-04-03 19:14:05 +0500

    Update 2023_11_13_143124_users_table.php

--- database/migrations/2023_11_13_143124_users_table.php
+++ database/migrations/2023_11_13_143124_users_table.php
@@ -14,10 +14,12 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-        $table->id();
-        $table->string('login');
-        $table->string('password');
-        $table->timestamps();
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
         });
     }
 
@@ -28,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::dropIfExists('users');
     }
 };
